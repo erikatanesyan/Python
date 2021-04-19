3a7a597752886400aab4d7909b487c8982f1c531
erikatanesyan <82462159+erikatanesyan@users.noreply.github.com>
2021-04-19 23:05:29 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+# Read the entered five-digit number. After that, each digit of this number must be printed on a new line.
+# mer_sirats_bar = "sombrero"
+# num_of_char = len(mer_sirats_bar)
+# print("count =", num_of_char)
+
+# Three numbers are given. Print "yes" if you can take any two of them and get the third in total
+# num1 = 2
+# num2 = 3
+# num3 = 5
+# if num1 + num2 == num3 or num1 + num3 == num2 or num2 + num3 == num1:
+    # print("Yes")
+# else:
+   # print("No")
+
+# Calculate the sum of a number series from 0 to 14 inclusive via loop. For example, 0 + 1 + 2 + 3 +… + 14;
+#num = 0
+#for a in range(0, 15):
+    #num += a
+#print(num)
+
+# Read the entered five-digit number. After that, each digit of this number must be printed on a new line
+#num = 55555
+#while num != 0:
+   #a = num % 10
+   #b = a
+   #num = num // 10
+   #print(b)
+
+# Print five lines of zeros in a loop, each line must be numbered
+#for a in range (1, 6):
+    #print (a,"000000")
