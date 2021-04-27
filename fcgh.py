91941916b115a0f39c5d886e1459543b4364f79c
erikatanesyan <82462159+erikatanesyan@users.noreply.github.com>
2021-04-27 11:43:07 +0400

    Add files via upload

--- fcgh.py
+++ fcgh.py
@@ -1,18 +1,30 @@
-# write a function that computes the volume of a sphere given its radius
-pi = 3.14
-radius = 1
-volume = (4/3)*pi*(radius**3)
-print("The Volume of the sphere is:", volume)
+# Write a function that will return the factorial of a given number
+def factorial(number):
+    sum = 2
+    if 0 <= number <= 2:
+        sum = number
+    else:
+        for x in range(2, number, 1):
+            sum = sum * (x+1)
+    return sum
+print(factorial(3))
 
-# Write a function that checks whether a passed string is palindrome or not
-def palindrome(x):
-    return x == x[::-1]
-x = input ("Enter a string:")
-y = palindrome(x)
-if y:
-    print("String is Palindrome")
-else:
-    print("String is not Palindrome")
+# Write a function that will return the count of given character in the given string
+def char_num(string_1, char_1):
+    count = 0
+    for character in string_1:
+        if character == char_1:
+            count += 1
+    return count
+print(char_num("barev dez gegeckuhi", "b"))
 
-
-
+# Write a function that will return count of characters till the given one
+def a(string_2, char_2):
+    count = 0
+    for b in string_2:
+        if b != char_2:
+            count += 1
+        else:
+            break
+        return count
+print(a("ahbbbasdf", "b"))
